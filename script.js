2f85573d9f6007b35ae2821a1b5e7c01e90f7d8d
danzaky <danielsaenz-96@hotmail.com>
2023-06-13 13:04:47 -0500

    Generar tabla con filas y columnas ingresadas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Función para verificar si un año es bisiesto
 function checkLeapYear() {
   const yearInput = document.getElementById('yearInput');
   const leapYearResult = document.getElementById('leapYearResult');
@@ -5,7 +6,32 @@ function checkLeapYear() {
 
   if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
     leapYearResult.textContent = `${year} es un año bisiesto`
+  }else if (isNaN(year)){
+    leapYearResult.textContent = `Debes ingresar un año válido`
   }else {
     leapYearResult.textContent = `${year} no es un año bisiesto`
   }
+}
+
+//Función para generar una tabla con las columnas y filas ingresadas
+function generateTable() {
+  const rowsInput = document.getElementById('rowsInput');
+  const columnsInput = document.getElementById('columnsInput');
+  const tableContainer = document.getElementById('tableContainer');
+  const rows = parseInt(rowsInput.value);
+  const columns = parseInt(columnsInput.value);
+  let tableHTML = '<table>';
+
+  for (let i = 0; i < rows; i++) {
+    tableHTML += '<tr>';
+
+    for (let j = 0; j < columns; j++) {
+      tableHTML += '<td></td>';
+    }
+
+    tableHTML += '</tr>';
+  }
+
+  tableHTML += '</table>';
+  tableContainer.innerHTML = tableHTML;
 }
